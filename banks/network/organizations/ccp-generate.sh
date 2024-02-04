775d3074b6ba378b2b76a5901e0a62b805581c79
Ratko Ruzicic <ratko.ruzicic@oaklandgrp.com>
2024-02-04 20:08:22 +0100

    Create crypto configs

--- banks/network/organizations/ccp-generate.sh
+++ banks/network/organizations/ccp-generate.sh
@@ -7,7 +7,10 @@ function one_line_pem {
 }
 
 function json_ccp {
-    local PP=$(one_line_pem $4)
+    local P0P=$(one_line_pem $4)
+    local P1P=$(one_line_pem $4)
+    local P2P=$(one_line_pem $4)
+    local P3P=$(one_line_pem $4)
     local CP=$(one_line_pem $5)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
@@ -19,6 +22,10 @@ function json_ccp {
 
 function yaml_ccp {
     local PP=$(one_line_pem $4)
+    local P0P=$(one_line_pem $4)
+    local P1P=$(one_line_pem $4)
+    local P2P=$(one_line_pem $4)
+    local P3P=$(one_line_pem $4)
     local CP=$(one_line_pem $5)
     sed -e "s/\${ORG}/$1/" \
         -e "s/\${P0PORT}/$2/" \
@@ -30,6 +37,9 @@ function yaml_ccp {
 
 ORG=1
 P0PORT=7051
+P1PORT=7051
+P2PORT=7051
+P3PORT=7051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org1.example.com/tlsca/tlsca.org1.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org1.example.com/ca/ca.org1.example.com-cert.pem
@@ -39,6 +49,9 @@ echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrga
 
 ORG=2
 P0PORT=9051
+P1PORT=9051
+P2PORT=9051
+P3PORT=9051
 CAPORT=8054
 PEERPEM=organizations/peerOrganizations/org2.example.com/tlsca/tlsca.org2.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org2.example.com/ca/ca.org2.example.com-cert.pem
@@ -48,6 +61,9 @@ echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrga
 
 ORG=3
 P0PORT=7051
+P1PORT=7051
+P2PORT=7051
+P3PORT=7051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org3.example.com/tlsca/tlsca.org3.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org3.example.com/ca/ca.org3.example.com-cert.pem
@@ -57,6 +73,9 @@ echo "$(yaml_ccp $ORG $P0PORT $CAPORT $PEERPEM $CAPEM)" > organizations/peerOrga
 
 ORG=4
 P0PORT=7051
+P1PORT=7051
+P2PORT=7051
+P3PORT=7051
 CAPORT=7054
 PEERPEM=organizations/peerOrganizations/org4.example.com/tlsca/tlsca.org4.example.com-cert.pem
 CAPEM=organizations/peerOrganizations/org4.example.com/ca/ca.org4.example.com-cert.pem
